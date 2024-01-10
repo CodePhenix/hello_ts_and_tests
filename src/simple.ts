ac6e6489e5467683efcb897ee29721c303a20175
Zina A <keiraayari@gmail.com>
2024-01-10 10:14:22 +0100

    exercice sur simple.ts refait

--- src/simple.ts
+++ src/simple.ts
@@ -6,47 +6,57 @@ export function sum(a: number, b: number): number {
 
 export function double(a: number): number {
   return a * 2;
+
+
 }
 
 
 
 export function triple(a: number): number {
   return a * 3;
+
 }
 
 export function minutesToSecond(a: number): number {
   return a * 60;
+
 }
 
 export function mult(a: number, b: number): number {
+
   return a * b;
+
+
 }
 
 export function max(a: number, b: number): number {
   if (a > b) {
-    return a;
-  } else if (b > a) {
-    return b;
+    return a
   } else {
     return b;
+
   }
+
+
 }
 
 
 type Saison = "été" | "automne" | "hiver" | "printemps"
 export function saison(a: number): Saison {
-  if (a <= 1) {
-    return "hiver";
+  if (a <= 3) {
+    return "hiver"
 
-  } else if (a >= 3) {
-    return "printemps";
-
-  } else if (a >= 6) {
-    return "été";
+  } else if (a <= 6) {
+    return "printemps"
+  }
+  else if (a <= 9) {
+    return "été"
 
   } else {
-    return "automne";
 
+    return "automne";
   }
+
+
 }
 
