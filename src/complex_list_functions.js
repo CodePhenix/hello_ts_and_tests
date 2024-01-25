862c00d0e84e4fa38622bbb59c3684d599c7fffd
Zina A <keiraayari@gmail.com>
2024-01-25 12:49:50 +0100

    rajout exo computerFullName

--- src/complex_list_functions.js
+++ src/complex_list_functions.js
@@ -61,14 +61,14 @@ console.log(keepMineurs(data));
 const botte = [1, 2, 4]
 const une_aiguille = 6
 function estDansListe(botte, une_aiguille) {
-    for (const el of botte ){
-        if (une_aiguille == el){
-             return true; 
+    for (const el of botte) {
+        if (une_aiguille == el) {
+            return true;
         }
-       
-    } 
-    return false;     
-} 
+
+    }
+    return false;
+}
 
 console.log(estDansListe(botte, une_aiguille))
 
@@ -76,51 +76,98 @@ console.log(estDansListe(botte, une_aiguille))
 
 const idf = [75, 91, 92, 93, 94, 95, 77, 78];
 
-function keepIleDeFrance(humans){
+function keepIleDeFrance(humans) {
     let francil = [];
     for (const human of humans) {
         // if human.zipcode est dans idf 
         if (estDansListe(idf, human.zipcode)) {
-        //  console.log(human);
-        
+            //  console.log(human);
+
             francil.push(human);
-        }    
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-     return francil;
+        }
+    }
+    return francil;
 }
 
-function keepIleDeFrance2(humans){
+function keepIleDeFrance2(humans) {
     let francil = [];
     for (const human of humans) {
         // if human.zipcode est dans idf 
         if (idf.includes(human.zipcode)) {
-        //  console.log(human);
-        
+            //  console.log(human);
+
             francil.push(human);
-        }    
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-     return francil;
+        }
+    }
+    return francil;
 }
 
 console.log(keepIleDeFrance2(data));
 
-// filter if name starts with search variable
-// filterByName(data, "Tom") --> [
-//   { name: "Tommy", age: 17, zipcode: 42 },
-//   { name: "Tom", age: 76, zipcode: 93 },
+// // filter if name starts with search variable
+// // filterByName(data, "Tom")-- > [
+//     { name: "Tommy", age: 17, zipcode: 42 },
+//     { name: "Tom", age: 76, zipcode: 93 },
 // ];
 
-// const data_bis: ? = [
-//     { firstname: "Zoe", lastname: "Dupont" },
-//     { firstname: "Tommy", lastname: "Toto" },
-//     { firstname: "Joe", lastname: "Jojo" },
-//     { firstname: "Tom", lastname: "Titi" },
-//   ];
-  
-  // computeFullName(data_bis) -->
-  // [
-  //   { fullname: "Zoe Dupont" },
-  //   { fullname: "Tommy Toto" },
-  //   { fullname: "Joe Jojo" },
-  //   { fullname: "Tom Titi" },
-  // ]
+const data_ter = [
+    { name: "Zoe", age: 12, zipcode: 75 }, // human
+    { name: "Tommy", age: 17, zipcode: 42 },
+    { name: "Joe", age: 18, zipcode: 75 },
+    { name: "Tom", age: 76, zipcode: 93 },
+];
+
+function filterByName(humans, str1) {
+    let humanNames = [];
+    // if humanName contient "Tom" est dans la data
+    for (const human of humans) {
+        console.log("log", human.name, str1.includes(human.name))
+        if (human.name.includes(str1)) {
+            // (str1.startsWith(str1, humanNames.push(human)));
+            // on push human dans humanNames
+            humanNames.push(human);
+        };
+
+    }
+
+
+    // humanNames.push(humans);
+    return humanNames
+}
+console.log(filterByName(data_ter, "Tom"));
+
+// const entier = data_full.firstname.concat('', lastname);
+// const concat = concat("", "");
+const data_full = [
+    { firstname: "Zoe", lastname: "Dupont" },
+    { firstname: "Tommy", lastname: "Toto" },
+    { firstname: "Joe", lastname: "Jojo" },
+    { firstname: "Tom", lastname: "Titi" },
+];
+
+function computerFullName(humans) {
+    let fullNames = [];
+
+    for (const human of humans) {
+        // console.log(humans)
+        newHuman = human.firstname.concat(' ', human.lastname);
+        // newHuman = human.firstname + " " + human.lastname
+        // console.log(newHuman);
+
+        // fullNames.push(('fullname:'), (newHuman));
+
+        fullNames.push({ 'fullname': newHuman });
+    };
+
+
+    return fullNames
+}
+console.log(computerFullName(data_full));
+
+// computeFullName(data_bis)-- >
+//     [
+//         { fullname: "Zoe Dupont" },
+//         { fullname: "Tommy Toto" },
+//         { fullname: "Joe Jojo" },
+//         { fullname: "Tom Titi" },
+//     ]
